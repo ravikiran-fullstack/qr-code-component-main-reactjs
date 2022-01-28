6b6cf592f668094076f63d1ae4fe43a67e25d58e
ravikiran <ravikiransjce.jobs@gmail.com>
2022-01-28 13:54:49 +0530

    Added basic react and css layout, need to work on responsiveness

---
+++ src/components/qrCode/QRCode.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./QRCode.css";
+
+const QRCode = () => {
+  return (
+    <div className='qrCodeCmp'>
+      <div className='qrCodeCmpImg'></div>
+      <div>
+        <h3 className='heading'>
+          Improve your front-end skills by building projects
+        </h3>
+        <p>
+          Scan the QR cide to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default QRCode;
